021505c4975ca8f92303bf1d76a0a5bfe61ab9e9
igitonga <iangitonga47@gmail.com>
2025-03-05 18:04:53 +0300

    Some clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
@@ -13,14 +13,12 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Header />
         <Toaster />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-          <Route path="/add/recipe" element={<AddRecipe />} />
-        </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add/recipe" element={<AddRecipe />} />
       </div>
   );
 }
